d40263b83301abeb92cbe4435e8ea8a4fc18eab4
Bryce <brycewaltonmail@gmail.com>
2026-03-23 16:36:46 -0400

    bindgen sensor.h for autogenerating comboboxes for enum configuration

--- shared/build.rs
+++ shared/build.rs
@@ -1,16 +1,41 @@
+#[derive(Debug)]
+struct AddDerives;
+
+impl bindgen::callbacks::ParseCallbacks for AddDerives {
+    fn add_derives(&self, info: &bindgen::callbacks::DeriveInfo<'_>) -> Vec<String> {
+        if matches!(info.kind, bindgen::callbacks::TypeKind::Enum) {
+            vec![
+                "strum::VariantArray".to_string(),
+                "strum::EnumIter".to_string(),
+                "strum::Display".to_string(),
+            ]
+        } else {
+            vec![]
+        }
+    }
+}
+
 fn main() {
-    let header = "ksu-capstone-embedded/shared/shared_lib.h";
+    const SUBMODULE: &str = "ksu-capstone-embedded";
+    let headers = vec![
+        format!("{SUBMODULE}/shared/shared_lib.h"),
+        format!("{SUBMODULE}/managed_components/espressif__esp32-camera/driver/include/sensor.h"),
+    ];
 
-    println!("cargo:rerun-if-changed={header}");
-
-    let bindings = bindgen::Builder::default()
-        .header(header)
+    let mut builder = bindgen::Builder::default()
         .derive_debug(true)
         .derive_default(true)
         .derive_eq(true)
         .derive_partialeq(true)
-        .generate()
-        .expect("Unable to generate bindings from shared_lib.h");
+        .layout_tests(false)
+        .parse_callbacks(Box::new(AddDerives))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()));
+
+    for header in &headers {
+        builder = builder.header(header);
+    }
+
+    let bindings = builder.generate().expect("Unable to generate bindings");
 
     let out = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     bindings
